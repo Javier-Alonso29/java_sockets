4f83d8af14ad213c24f8bcdd95bb96055c34f99e
Javier Alonso <60083810+Javier-Alonso29@users.noreply.github.com>
2020-06-20 19:02:17 -0500

    Agrego GUI al proyecto Java_sockets

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author Carlos Javier Alonso Caldera
+ * @author Carlos Vela de la Rosa
+ */
+public class Cliente {
+    private String nombre;
+    private String ip;
+    private String puerto;
+    
+}
